f01efda5405e3fe9fb6255281dec72e462dbb05a
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-12-15 17:16:38 +0800

    feat: add data and model builders

---
+++ src/model_builder.py
@@ -0,0 +1,52 @@
+"""
+Contains PyTorch model code to instantiate a TinyVGG model.
+"""
+
+import torch
+from torch import nn
+
+
+class TinyVGG(nn.Module):
+    """Creates the TinyVGG architecture.
+
+    Replicates the TinyVGG architecture from the CNN explainer website in PyTorch.
+    See the original architecture here: https://poloclub.github.io/cnn-explainer/
+
+    Args:
+        input_shape: An integer indicating number of input channels.
+        hidden_units: An integer indicating number of hidden units between layers.
+        output_shape: An integer indicating number of output units.
+    """
+
+    def __init__(
+        self,
+        input_shape: int,
+        hidden_units: int,
+        output_shape: int,
+        image_length: int,
+    ):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(input_shape, hidden_units, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(hidden_units * pow(int(image_length / 4), 2), output_shape),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.classifier(x)
+        return x
